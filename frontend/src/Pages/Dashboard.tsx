25b13afeef7327db06016df0b5c1e2b49edf234b
Milinbhakta <desire2dosomething@gmail.com>
2023-12-21 20:55:01 -0400

    Refactor Dashboard component to use Material-UI components

--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -1,16 +1,28 @@
 import { Link } from 'react-router-dom';
-import { Container, Header, List } from 'semantic-ui-react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 
 const Dashboard = () => {
   const content = (
     <Container>
-      <Header as="h1">Dashboard</Header>
-      <Header as="h4">Welcome to Dashboard! {new Date().toString()}</Header>
+      <Typography variant="h4">Dashboard</Typography>
+      <Typography variant="h6">
+        Welcome to Dashboard! {new Date().toString()}
+      </Typography>
       {/* Link to view Tasks */}
-      <List bulleted>
-        <List.Item as="h3">
-          <Link to="/tasks">View your tasks</Link>
-        </List.Item>
+      <List>
+        <ListItem>
+          <ListItemText
+            primary={
+              <Typography variant="h6">
+                <Link to="/tasks">View your tasks</Link>
+              </Typography>
+            }
+          />
+        </ListItem>
       </List>
     </Container>
   );
