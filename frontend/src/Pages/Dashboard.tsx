e4989e7e75d66c48904c9acd230ce3054f0efd51
Milinbhakta <desire2dosomething@gmail.com>
2023-12-22 16:45:28 -0400

    Refactor Dashboard component to use MUI List and ListItem

--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -1,35 +1,45 @@
 import Container from '@mui/material/Container';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
+import { useTheme } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
+  const theme = useTheme();
+
   const content = (
     <Container>
       <Typography variant="h4">Dashboard</Typography>
       <Typography variant="h6">
         Welcome to Dashboard! {new Date().toString()}
       </Typography>
-      <Stack
-        direction="column"
-        justifyContent="space-evenly"
-        alignItems="flex-start"
-        spacing={2}
-        sx={{ mt: 2 }}
-      >
-        <Link
-          to="/tasks"
-          style={{ color: 'white', cursor: 'pointer', textDecoration: 'none' }}
-        >
-          &#8226; View Tasks
-        </Link>
-        <Link
-          to="/tasks/create"
-          style={{ color: 'white', cursor: 'pointer', textDecoration: 'none' }}
-        >
-          &#8226; Create Task
-        </Link>
-      </Stack>
+      <List sx={{ mt: 2 }}>
+        <ListItem>
+          <Link
+            to="/tasks"
+            style={{
+              color: theme.palette.mode === 'dark' ? 'white' : 'black',
+              cursor: 'pointer',
+              textDecoration: 'none',
+            }}
+          >
+            &#8226; View Tasks
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link
+            to="/tasks/create"
+            style={{
+              color: theme.palette.mode === 'dark' ? 'white' : 'black',
+              cursor: 'pointer',
+              textDecoration: 'none',
+            }}
+          >
+            &#8226; Create Task
+          </Link>
+        </ListItem>
+      </List>
     </Container>
   );
   return content;
