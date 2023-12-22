2b4b9086ff8f1a15a9bf1c8762f065afdb98a693
Milinbhakta <desire2dosomething@gmail.com>
2023-12-22 17:37:06 -0400

    Add new task form component

--- frontend/src/Pages/Tasks/NewTask.tsx
+++ frontend/src/Pages/Tasks/NewTask.tsx
@@ -1,5 +1,183 @@
 // NewTask.tsx
 
+import {
+  Stack,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Box,
+  Button,
+  CircularProgress,
+  SelectChangeEvent,
+} from '@mui/material';
+import Container from '@mui/material/Container';
+import axiosInstance from '../../Utils/AxiosInstance';
+import { useEffect, useState } from 'react';
+import { Task, Status } from '../../Utils/Types';
+import { useMessage } from '../../hooks/MessageContext';
+import { useKeycloak } from '../../hooks/KeycloakContext';
+
 export default function NewTask() {
-  return <div>NewTask</div>;
+  const { keycloak } = useKeycloak();
+  const [task, setTask] = useState<Task>({
+    task_name: '',
+    description: '',
+    deadline: '',
+    status_id: 1,
+    assigned_to: keycloak?.tokenParsed?.sub || '',
+    created_on: '',
+    last_update: '',
+    task_id: 0,
+  });
+  const [statuses, setStatuses] = useState<Status[] | null>(null);
+  const { showMessage } = useMessage();
+
+  useEffect(() => {
+    axiosInstance('/status')
+      .then((response) => response.data)
+      .then((data) => setStatuses(data));
+  }, []);
+
+  if (!statuses) {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '800px',
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await axiosInstance.post(`/tasks/`, {
+        ...task,
+      });
+
+      if (response.status === 200) {
+        showMessage('Task updated successfully', 'success');
+        // Clear the form
+        setTask({
+          task_name: '',
+          description: '',
+          deadline: '',
+          status_id: 1,
+          assigned_to: keycloak?.tokenParsed?.sub || '',
+          created_on: '',
+          last_update: '',
+          task_id: 0,
+        });
+      } else {
+        showMessage('Error updating task', 'error');
+      }
+    } catch (error) {
+      console.error(error);
+      showMessage('Error updating task', 'error');
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name as keyof typeof task;
+    const value = e.target.value;
+
+    setTask({
+      ...task,
+      [name]: value,
+    });
+  };
+
+  const handleSelectChange = (e: SelectChangeEvent) => {
+    const name = e.target.name as keyof typeof task;
+    const value = Number(e.target.value);
+
+    setTask({
+      ...task,
+      [name]: value,
+    });
+  };
+
+  return (
+    <Container sx={{ marginTop: 3 }}>
+      <form onSubmit={handleSubmit}>
+        <Stack display="flex" direction="column" spacing={3}>
+          <TextField
+            label="Task Name"
+            name="task_name"
+            placeholder="Task Name"
+            value={task.task_name}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Description"
+            name="description"
+            placeholder="Task Description"
+            value={task.description}
+            onChange={handleInputChange}
+            multiline
+            fullWidth
+            margin="normal"
+          />
+          <FormControl fullWidth>
+            <InputLabel id="Status">Status</InputLabel>
+            <Select
+              label="Status"
+              name="status_id"
+              value={task.status_id.toString()}
+              onChange={handleSelectChange}
+              fullWidth
+              MenuProps={{
+                anchorOrigin: {
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                },
+                transformOrigin: {
+                  vertical: 'top',
+                  horizontal: 'left',
+                },
+              }}
+            >
+              {statuses.map((status) => (
+                <MenuItem key={status.id} value={status.id.toString()}>
+                  {status.description}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <TextField
+            name="deadline"
+            label="Deadline"
+            type="date"
+            value={task.deadline}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <Box display="flex" justifyContent="flex-end">
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              size="medium"
+            >
+              Submit
+            </Button>
+          </Box>
+        </Stack>
+      </form>
+    </Container>
+  );
 }
