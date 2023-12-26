5343034a2260aa5aacb99362751ccae1469446ae
Milinbhakta <desire2dosomething@gmail.com>
2023-12-25 23:35:00 -0400

    Update BrowserRouter basename in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProviderWrapper>
       <CssBaseline />
       <MessageProvider>
-        <BrowserRouter basename="/">
+        <BrowserRouter>
           <Routes>
             <Route path="/*" element={<App />} />
           </Routes>
